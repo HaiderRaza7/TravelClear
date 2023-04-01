e8013b9c06044111ba0b276b2d6d3c2654511096
HaiderRaza7 <Haider-raza77@hotmail.com>
2023-04-01 18:38:59 -0400

    Test version of site

--- MapHacks/app.py
+++ MapHacks/app.py
@@ -10,7 +10,6 @@ OPENTRIPMAP_ENDPOINT = "https://api.opentripmap.com/0.1/en/places/bbox"
 
 app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
-
 def index():
     if request.method == 'POST':
         # Handle form submission
@@ -18,7 +17,7 @@ def index():
         data = travel_clear(location)
         return render_template('index.html', data=data)
     else:
-        return render_template('index.html')
+        return render_template('index.html', error=True)
 
 
 def travel_clear(location):
