1481c9a3d76352fb80a07146ff2f423655c15ab5
HaiderRaza7 <Haider-raza77@hotmail.com>
2023-04-01 19:20:52 -0400

    Added 'Surprise me!' button functionality
    Also tried to fix accommodations. Didn't work.

--- MapHacks/app.py
+++ MapHacks/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, send_file, jsonify
+from flask import Flask, render_template, request
 import requests
 import json
+import random
 
 # Set up API keys and endpoints
 OPENWEATHERMAP_API_KEY = "6b6c7dac6f7616e218d554848d9f56c1"
@@ -11,12 +12,30 @@ OPENTRIPMAP_ENDPOINT = "https://api.opentripmap.com/0.1/en/places/bbox"
 
 app = Flask(__name__)
 
+cities = ['Paris', 'New York City', 'Rome', 'London', 'Dubai', 'Barcelona', 'Bangkok', 'Singapore', 'Istanbul',
+          'Tokyo', 'Hong Kong', 'Amsterdam', 'Vienna', 'Prague', 'Los Angeles', 'Marrakesh', 'Shanghai', 'Berlin',
+          'Madrid', 'Miami', 'Sydney', 'Florence', 'Buenos Aires', 'Toronto', 'San Francisco', 'Las Vegas',
+          'Rio de Janeiro', 'Mexico City', 'Cape Town', 'Dublin', 'Edinburgh', 'Vancouver', 'Stockholm', 'Brussels',
+          'Munich', 'Budapest', 'Copenhagen', 'Krakow', 'Lisbon', 'Zurich', 'Athens', 'Honolulu', 'Hanoi', 'San Diego',
+          'Osaka', 'Washington D.C.', 'Seoul', 'St. Petersburg', 'Dubrovnik', 'Venice', 'Montreal', 'Jerusalem',
+          'Kyoto', 'Sao Paulo', 'Beijing', 'Nairobi', 'Hamburg', 'Naples', 'Abu Dhabi', 'Cairo', 'Cartagena',
+          'Seville', 'Cusco', 'Jakarta', 'Salzburg', 'Stavanger', 'Bordeaux', 'Cologne', 'Queenstown', 'Reykjavik',
+          'Kruger National Park', 'Valletta', 'Cannes', 'Santiago', 'Melbourne', 'Santorini', 'Split', 'Oslo',
+          'Marseille', 'Auckland', 'Lucerne', 'Geneva', 'Granada', 'Bologna', 'Brisbane', 'Tallinn', 'Krakow',
+          'Munich', 'San Sebastian', 'Bruges', 'Pula', 'Siem Reap', 'Zanzibar', 'Helsinki', 'Plovdiv', 'La Paz',
+          'Beirut', 'Lviv', 'Quebec City', 'Baku', 'Minsk']
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        # Handle form submission
-        location = request.form['text_input'].title()
+        if 'submit' in request.form:
+            # Handle form submission
+            location = request.form['text_input'].title()
+        elif 'random' in request.form:
+            location = random.choice(cities)
+        else:
+            return render_template('index.html', error=True)
         data = travel_clear(location)
         return render_template('index.html', data=data)
     else:
@@ -37,10 +56,19 @@ def travel_clear(location):
     params = {"apikey": OPENTRIPMAP_API_KEY, "bbox": get_bbox(location)}
     response = requests.get(OPENTRIPMAP_ENDPOINT, params=params)
     accommodations_data = response.json()
-    print("Accommodations data:", accommodations_data)
+    # Iterate through the accommodations data and extract relevant information
+    accommodations_list = accommodations_data.get("accommodations", [])
+    print(accommodations_list)
+    accommodations_info = []
+    for accommodation in accommodations_list:
+        name = accommodation.get("name", "Unknown")
+        address = accommodation.get("address", "Unknown")
+        rating = accommodation.get("rate", {}).get("rating", "Unknown")
+        accommodations_info.append(f"{name} - {address}, Rating: {rating}")
 
-    # Convert JSON data to strings
-    accommodations_str = jsonify(accommodations_data)
+    # Convert the accommodations information into a readable string format
+    accommodations_info = "\n".join(accommodations_info)
+    accommodations_str = f"Accommodations in {location}:\n{accommodations_info}"
 
     # Return the string data
     return {"Weather data": weather_str, "Accommodations data": accommodations_str}
