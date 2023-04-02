ea9518167fca82a87441b3cc53cb03e8f178efda
HaiderRaza7 <Haider-raza77@hotmail.com>
2023-04-01 23:18:23 -0400

    Website now outputs description and tourist spots for location
    Powered by chatGPT via openai API

--- MapHacks/app.py
+++ MapHacks/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 import requests
 import json
 import random
+import openai
 
 # Set up API keys and endpoints
 OPENWEATHERMAP_API_KEY = "6b6c7dac6f7616e218d554848d9f56c1"
@@ -11,6 +12,9 @@ OPENWEATHERMAP_ENDPOINT = "http://api.openweathermap.org/data/2.5/weather"
 # OPENTRIPMAP_ENDPOINT = "https://api.opentripmap.com/0.1/en/places/bbox"
 
 app = Flask(__name__)
+
+openai.api_key = "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"  # Do not alter!
+model = "text-davinci-002"  # The ChatGPT model to use
 
 cities = ['Paris', 'New York City', 'Rome', 'London', 'Dubai', 'Barcelona', 'Bangkok', 'Singapore', 'Istanbul',
           'Tokyo', 'Hong Kong', 'Amsterdam', 'Vienna', 'Prague', 'Los Angeles', 'Marrakesh', 'Shanghai', 'Berlin',
@@ -37,7 +41,7 @@ def index():
         else:
             return render_template('index.html', error=True)
         data = travel_clear(location)
-        return render_template('index.html', data=data)
+        return render_template('index.html', weather=data[0], description=data[1])
     else:
         return render_template('index.html', error=True)
 
@@ -49,45 +53,24 @@ def travel_clear(location):
     weather_data = json.loads(response.text)
     temperature = round(weather_data["main"]["temp"] - 273.15, 1)
     weather_description = weather_data["weather"][0]["description"]
-    weather_str = f"The temperature in {location} is {temperature} degrees Celsius, and the weather is" \
-                  f" {weather_description}."
+    weather = f"The temperature in {location} is {temperature} degrees Celsius, and the weather is" \
+              f" {weather_description}."
 
-    # # Use OpenTripMap API to get accommodations data
-    # params = {"apikey": OPENTRIPMAP_API_KEY, "bbox": get_bbox(location)}
-    # response = requests.get(OPENTRIPMAP_ENDPOINT, params=params)
-    # accommodations_data = response.json()
-    # # Iterate through the accommodations data and extract relevant information
-    # accommodations_list = accommodations_data.get("accommodations", [])
-    # print(accommodations_list)
-    # accommodations_info = []
-    # for accommodation in accommodations_list:
-    #     name = accommodation.get("name", "Unknown")
-    #     address = accommodation.get("address", "Unknown")
-    #     rating = accommodation.get("rate", {}).get("rating", "Unknown")
-    #     accommodations_info.append(f"{name} - {address}, Rating: {rating}")
+    prompt = f'Please give a description of {location} with a quick description of its history and culture.' \
+             f' Please also give a list of tourist or famous spots to visit while visiting this location.'
 
-    # # Convert the accommodations information into a readable string format
-    # accommodations_info = "\n".join(accommodations_info)
-    # accommodations_str = f"Accommodations in {location}:\n{accommodations_info}"
+    response = openai.Completion.create(
+        engine=model,
+        prompt=prompt,
+        max_tokens=100,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
 
-    #, "Accommodations data": accommodations_str
-
-    accommodations_str = "you should see something about accommodations here"
-    # Return the string data
-    return {"Weather data": weather_str}
-
-
-# def get_bbox(location):
-#     # Use Nominatim API to get coordinates of the location
-#     nominatim_endpoint = "https://nominatim.openstreetmap.org/search"
-#     params = {"q": location, "format": "json"}
-#     response = requests.get(nominatim_endpoint, params=params)
-#     data = response.json()
-#     lat = data[0]["lat"]
-#     lon = data[0]["lon"]
-#     # Calculate the bounding box
-#     bbox = f"{float(lon)-0.5},{float(lat)-0.5},{float(lon)+0.5},{float(lat)+0.5}"
-#     return bbox
+    description = response.choices[0].text
+    print(description)
+    return weather, description
 
 
 if __name__ == "__main__":
