68d012116fcfcb05d33db324130b72f5c4affe59
Nyi Htet <nyihtet4dev@icloud.com>
2023-04-01 19:41:33 -0700

    first commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 import React from "react";
-
+import Search from "./pages/Search/Search";
+import "bootstrap/dist/css/bootstrap.min.css";  //npm install bootstrap
+import "bootstrap/dist/js/bootstrap";
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <div className="mx-auto">App</div>
+      <Search/>
+      </div>
+    );
 }
 
 export default App;
