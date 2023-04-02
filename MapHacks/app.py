e0a742f774183d20964505df2f1fb0330d7d9113
HaiderRaza7 <Haider-raza77@hotmail.com>
2023-04-02 00:25:04 -0400

    Changed location to city

--- MapHacks/app.py
+++ MapHacks/app.py
@@ -14,7 +14,7 @@ model = "text-davinci-002"  # The ChatGPT model to use
 
 app = Flask(__name__)
 
-# 100 of the most popular tourist attractions. Used to "surprise" the user with a random location
+# 100 of the most popular tourist attractions. Used to "surprise" the user with a random city
 cities = ['Paris', 'New York City', 'Rome', 'London', 'Dubai', 'Barcelona', 'Bangkok', 'Singapore', 'Istanbul',
           'Tokyo', 'Hong Kong', 'Amsterdam', 'Vienna', 'Prague', 'Los Angeles', 'Marrakesh', 'Shanghai', 'Berlin',
           'Madrid', 'Miami', 'Sydney', 'Florence', 'Buenos Aires', 'Toronto', 'San Francisco', 'Las Vegas',
@@ -32,42 +32,42 @@ cities = ['Paris', 'New York City', 'Rome', 'London', 'Dubai', 'Barcelona', 'Ban
 @app.route('/', methods=['GET', 'POST'])
 def index():
     """
-    This function is called when the user submits their location input (or if they click the Surprise me button).
+    This function is called when the user submits their city input (or if they click the Surprise me button).
     It scrapes the web using open weather map API and utilizes openai API (chatGPT) to get important information about
-    the location's weather and popular tourist spots in addition to providing a brief introduction for the location.
-    :return: Weather and description about the location which is to be sent to index.html to be displayed to the user.
+    the city's weather and popular tourist spots in addition to providing a brief introduction for the city.
+    :return: Weather and description about the city which is to be sent to index.html to be displayed to the user.
     """
     if request.method == 'POST':
         if 'submit' in request.form:
             # Handle form submission
-            location = request.form['text_input'].title()
+            city = request.form['text_input'].title()
         elif 'random' in request.form:
-            location = random.choice(cities)
+            city = random.choice(cities)
         else:
             return render_template('index.html', error=True)
-        description, tourist_spots, weather = travel_clear(location)
+        description, tourist_spots, weather = travel_clear(city)
         return render_template('index.html', description=description, tourist_spots=tourist_spots, weather=weather)
     else:
         return render_template('index.html', error=True)
 
 
-def travel_clear(location):
+def travel_clear(city):
     """
     Extract the information that index() needs to do its job.
-    :param location: city that we need to obtain information about.
-    :return: Weather, description, and tourist spots about the location.
+    :param city: city that we need to obtain information about.
+    :return: Weather, description, and tourist spots about the city.
     """
     # Use OpenWeatherMap API to get weather data
     time.sleep(30)
-    params = {"q": location, "appid": OPENWEATHERMAP_API_KEY}
+    params = {"q": city, "appid": OPENWEATHERMAP_API_KEY}
     response = requests.get(OPENWEATHERMAP_ENDPOINT, params=params)
     weather_data = json.loads(response.text)
     temperature = round(weather_data["main"]["temp"] - 273.15, 1)
     weather_description = weather_data["weather"][0]["description"]
-    weather = f"The temperature in {location} is {temperature} degrees Celsius, and the weather is" \
+    weather = f"The current temperature in {city} is {temperature} degrees Celsius, and the current weather is" \
               f" {weather_description}."
 
-    prompt = f'Please give a description of the location, history, and culture of {location}.' \
+    prompt = f'Please give a description of the location, history, and culture of {city}.' \
              f'You must complete sentences that you start.'
 
     response = openai.Completion.create(
@@ -81,7 +81,7 @@ def travel_clear(location):
     description = response.choices[0].text
     description = description[:description.rindex('.') + 1]
 
-    prompt = f'Please give a list of tourist spots in {location}, but you must separate them with a comma. ' \
+    prompt = f'Please give a list of tourist spots in {city}, but you must separate them with a comma. ' \
              f'For example, if you would like to list 3 locations called A, B, and C then please output them ' \
              f'exactly in this format: A,B,C.' \
              f'You may list as many as you like as long as they are worth visiting.'
