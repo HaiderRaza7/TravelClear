308be7b5cc5c8bbfd960ba17c766d72597c62d79
Jacky Wang <wangjacky991@gmail.com>
2023-04-01 19:36:30 -0600

    Commented out OpenTripMap code

--- MapHacks/app.py
+++ MapHacks/app.py
@@ -7,8 +7,8 @@ import random
 OPENWEATHERMAP_API_KEY = "6b6c7dac6f7616e218d554848d9f56c1"
 OPENWEATHERMAP_ENDPOINT = "http://api.openweathermap.org/data/2.5/weather"
 
-OPENTRIPMAP_API_KEY = "5ae2e3f221c38a28845f05b67fbd6206c69e2f737a4108db90894f98"
-OPENTRIPMAP_ENDPOINT = "https://api.opentripmap.com/0.1/en/places/bbox"
+# OPENTRIPMAP_API_KEY = "5ae2e3f221c38a28845f05b67fbd6206c69e2f737a4108db90894f98"
+# OPENTRIPMAP_ENDPOINT = "https://api.opentripmap.com/0.1/en/places/bbox"
 
 app = Flask(__name__)
 
@@ -52,39 +52,39 @@ def travel_clear(location):
     weather_str = f"The temperature in {location} is {temperature} degrees Celsius, and the weather is" \
                   f" {weather_description}."
 
-    # Use OpenTripMap API to get accommodations data
-    params = {"apikey": OPENTRIPMAP_API_KEY, "bbox": get_bbox(location)}
-    response = requests.get(OPENTRIPMAP_ENDPOINT, params=params)
-    accommodations_data = response.json()
-    # Iterate through the accommodations data and extract relevant information
-    accommodations_list = accommodations_data.get("accommodations", [])
-    print(accommodations_list)
-    accommodations_info = []
-    for accommodation in accommodations_list:
-        name = accommodation.get("name", "Unknown")
-        address = accommodation.get("address", "Unknown")
-        rating = accommodation.get("rate", {}).get("rating", "Unknown")
-        accommodations_info.append(f"{name} - {address}, Rating: {rating}")
+    # # Use OpenTripMap API to get accommodations data
+    # params = {"apikey": OPENTRIPMAP_API_KEY, "bbox": get_bbox(location)}
+    # response = requests.get(OPENTRIPMAP_ENDPOINT, params=params)
+    # accommodations_data = response.json()
+    # # Iterate through the accommodations data and extract relevant information
+    # accommodations_list = accommodations_data.get("accommodations", [])
+    # print(accommodations_list)
+    # accommodations_info = []
+    # for accommodation in accommodations_list:
+    #     name = accommodation.get("name", "Unknown")
+    #     address = accommodation.get("address", "Unknown")
+    #     rating = accommodation.get("rate", {}).get("rating", "Unknown")
+    #     accommodations_info.append(f"{name} - {address}, Rating: {rating}")
 
-    # Convert the accommodations information into a readable string format
-    accommodations_info = "\n".join(accommodations_info)
-    accommodations_str = f"Accommodations in {location}:\n{accommodations_info}"
+    # # Convert the accommodations information into a readable string format
+    # accommodations_info = "\n".join(accommodations_info)
+    # accommodations_str = f"Accommodations in {location}:\n{accommodations_info}"
 
     # Return the string data
     return {"Weather data": weather_str, "Accommodations data": accommodations_str}
 
 
-def get_bbox(location):
-    # Use Nominatim API to get coordinates of the location
-    nominatim_endpoint = "https://nominatim.openstreetmap.org/search"
-    params = {"q": location, "format": "json"}
-    response = requests.get(nominatim_endpoint, params=params)
-    data = response.json()
-    lat = data[0]["lat"]
-    lon = data[0]["lon"]
-    # Calculate the bounding box
-    bbox = f"{float(lon)-0.5},{float(lat)-0.5},{float(lon)+0.5},{float(lat)+0.5}"
-    return bbox
+# def get_bbox(location):
+#     # Use Nominatim API to get coordinates of the location
+#     nominatim_endpoint = "https://nominatim.openstreetmap.org/search"
+#     params = {"q": location, "format": "json"}
+#     response = requests.get(nominatim_endpoint, params=params)
+#     data = response.json()
+#     lat = data[0]["lat"]
+#     lon = data[0]["lon"]
+#     # Calculate the bounding box
+#     bbox = f"{float(lon)-0.5},{float(lat)-0.5},{float(lon)+0.5},{float(lat)+0.5}"
+#     return bbox
 
 
 if __name__ == "__main__":
